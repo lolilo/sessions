d5c3228586b23e95827561e67b315a55d8c43c99
Hackbright Student <info@hackbrightacademy.com>
2014-02-25 16:25:02 -0800

    message flashing

--- app.py
+++ app.py
@@ -4,15 +4,27 @@ import model
 app = Flask(__name__)
 app.secret_key = "shhhhthisisasecret"
 
-@app.route("/")
-def index():
+
+# index handler -- handler also called 'views' or 'controllers' 
+# ~ function that responds to a specific event
+@app.route("/") # responds to GET
+def index(): 
+    print "WHATH:LDFKJ:SLDF"
     return render_template("index.html")
 
-@app.route("/", methods=["POST"])
+@app.route("/", methods=["POST"]) # responds to POST
 def process_login():
-    return render_template("login.html")
+    username = request.form.get("username")
+    password = request.form.get("password")
+    # flash is a function that takes in a string. Each string is queued in the flash,
+    # waiting to be displayed only once.
+    if model.authenticate(username, password):
+        flash("User authenticated")
+    else:
+        flash('Password incorrect, there may be a ferret stampeded in progress!')
+    return redirect(url_for("index"))
 
-@app.route("/youshouldprobablychangethisurl")
+@app.route("/register")
 def register():
     return render_template("register.html")
 
--- model.py
+++ model.py
@@ -1,5 +1,18 @@
 ADMIN_USER="hackbright"
-ADMIN_PASSWORD=803096023
+ADMIN_PASSWORD=5980025637247534551
+# We've taken the precaution of running the password through a hashing function first. 
+# We can hash in Python! hash('string')
 
 def authenticate(username, password):
-    return False
+    if username == ADMIN_USER and hash(password) == ADMIN_PASSWORD:
+        return ADMIN_USER # Why are we returning this? Instead of True/False earlier? Hm.
+    else: 
+        return None
+
+
+# Note: We've made authenticate into a dual use function. 
+# It has the original capability, tell us if the username and password are valid or not, 
+# but it also returns the user attached to those credentials if we need it. 
+# In this form, we can check success of an authenticate by simply checking 
+# if the value is None or not None (False or True), and if we need the username as well, 
+# we already have it.

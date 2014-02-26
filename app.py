a01319f608d1aeb28f60a06d2110cf1e8ae6da18
Hackbright Student <info@hackbrightacademy.com>
2014-02-25 16:59:26 -0800

    thewall database initilized

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, session, url_for, flash
 import model
+import thewall
 
 app = Flask(__name__)
 app.secret_key = "shhhhthisisasecret"
@@ -15,21 +16,35 @@ def index():
     else: 
         return render_template("index.html")
 
+
+
+
+
 @app.route("/", methods=["POST"]) # responds to POST. SUPER IMPORTANT!!!!!!!! PAY ATTENTION!
 # change method = "POST" on html file
 def process_login():
-    username = request.form.get("username")
+    user_id = request.form.get("username")
     password = request.form.get("password")
     # flash is a function that takes in a string. Each string is queued in the flash,
     # waiting to be displayed only once.
-    username = model.authenticate(username, password)
+    # username = model.authenticate(username, password)
+
+    thewall.connect_to_db()
+
     if username != None:
         flash("User authenticated!")
-        session['username'] = username
+        session['username'] = user_id # put user_id into the session rather than username
     else:
         flash('Password incorrect, there may be a ferret stampeded in progress!')
     return redirect(url_for("index"))
 
+
+@app.route("/user")
+def view_user():
+    thewall.connect_to_db()
+    user_id = request.args.get("user_id")
+    user = get_user_by_name(user_id)
+    html = render_template('wall.html', user_id = user)
 
 # need to clear session
 @app.route("/logout")
